4ba41d12e849302b6fdb29f7f87ef936ee49e3ad
Vadim Volkov <antibiotik15@ya.ru>
2021-03-03 09:13:54 +0300

    fill AuthorService

--- src/main/java/by/volkov/testtask/service/AuthorService.java
+++ src/main/java/by/volkov/testtask/service/AuthorService.java
@@ -1,16 +1,50 @@
 package by.volkov.testtask.service;
 
+import by.volkov.testtask.model.Author;
 import by.volkov.testtask.repository.AuthorRepository;
+import by.volkov.testtask.service.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
 
-    private final AuthorRepository authorRepository;
+    private final AuthorRepository repository;
 
     @Autowired
-    public AuthorService(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
+    public AuthorService(AuthorRepository repository) {
+        this.repository = repository;
+    }
+
+    public Author create(Author author) {
+        Assert.notNull(author, "author must not be null");
+        return repository.save(author);
+    }
+
+    public void delete(int id) {
+        if (!repository.delete(id)) {
+            throw new NotFoundException("Not found entity");
+        }
+    }
+
+    public Author get(int id) {
+        if (repository.get(id) == null) {
+            throw new NotFoundException("Not found entity");
+        }
+        return repository.get(id);
+    }
+
+    public void update(Author author) {
+        Assert.notNull(author, "author must not be null");
+        if (repository.save(author) == null) {
+            throw new NotFoundException("Not found entity");
+        }
+    }
+
+    public List<Author> getAll() {
+        return repository.getAll();
     }
 }
---
+++ src/main/java/by/volkov/testtask/service/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package by.volkov.testtask.service.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
