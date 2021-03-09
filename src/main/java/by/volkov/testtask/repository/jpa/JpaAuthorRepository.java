f1477d517e46ac1b02fa3eb074bd0ec34b6c3426
Vadim Volkov <antibiotik15@ya.ru>
2021-03-09 13:14:00 +0300

    add Transactional

--- src/main/java/by/volkov/testtask/repository/jpa/JpaAuthorRepository.java
+++ src/main/java/by/volkov/testtask/repository/jpa/JpaAuthorRepository.java
@@ -3,18 +3,24 @@ package by.volkov.testtask.repository.jpa;
 import by.volkov.testtask.model.Author;
 import by.volkov.testtask.repository.AuthorRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
+// Why is @Transactional here?
+// https://www.ibm.com/developerworks/ru/library/j-ts1/
+// https://medium.com/@kirill.sereda/%D1%82%D1%80%D0%B0%D0%BD%D0%B7%D0%B0%D0%BA%D1%86%D0%B8%D0%B8-%D0%B2-spring-framework-a7ec509df6d2
 public class JpaAuthorRepository implements AuthorRepository {
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
+    @Transactional
     public Author save(Author author) {
         if (author.isNew()) {
             em.persist(author);
@@ -25,6 +31,7 @@ public class JpaAuthorRepository implements AuthorRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return em.createNamedQuery(Author.DELETE)
                 .setParameter("id", id)
